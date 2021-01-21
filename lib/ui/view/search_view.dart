5fe1b95cd6be82fe6fa42aed318d57275f803684
ShiryaevNikolay <deendeeps@list.ru>
2021-01-21 20:58:22 +0300

    Добавил кнопку удаления для поиска

--- lib/ui/view/search_view.dart
+++ lib/ui/view/search_view.dart
@@ -2,26 +2,25 @@ import 'package:film_catalog_app_flutter/domain/cubit/movie_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class SearchView extends StatelessWidget {
+class SearchView extends StatefulWidget {
 
+  @override
+  _SearchViewState createState() => _SearchViewState();
+}
+
+class _SearchViewState extends State<SearchView> {
   MovieCubit _cubit;
-  
-  var _inputDecoration = InputDecoration(
-                    hintText: "Поиск по названию фильма",
-                    prefixIcon: Icon(Icons.search),
-                    // suffixIcon: Icon(Icons.clear),
-                    filled: true,
-                    fillColor: Colors.white,
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.circular(100)
-                    )
-                  );
+
+  final _textSearchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
 
     _cubit = BlocProvider.of<MovieCubit>(context);
+
+    _textSearchController.addListener(() { 
+      _listenerText(_textSearchController.text);
+    });
 
     return Container(
       color: Colors.black,
@@ -37,9 +36,25 @@ class SearchView extends StatelessWidget {
   Expanded _buildTextField() {
     return Expanded(
       child: TextField(
-        onChanged: _listenerText,
+        controller: _textSearchController,
+        // onChanged: _listenerText,
         maxLines: 1,
-        decoration: _inputDecoration,
+        decoration: InputDecoration(
+                    hintText: "Поиск по названию фильма",
+                    prefixIcon: Icon(Icons.search),
+                    suffixIcon: _textSearchController.text.length > 0
+                      ? IconButton(
+                        icon: Icon(Icons.clear,), 
+                        onPressed: () { _textSearchController.clear(); }
+                      )
+                      : null,
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(100)
+                    )
+                  ),
       )
     );
   }
